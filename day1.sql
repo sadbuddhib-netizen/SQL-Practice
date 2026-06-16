82f095f7d241f225f945404f024b5ed2942cc902
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-16 20:56:56 +0530

    Add files via upload

---
+++ day1.sql
@@ -0,0 +1,18 @@
+
+use om;
+create table women(  id int,name varchar(50),age int,surname varchar(70),degree varchar(30));
+show tables;
+insert into women values(1,"sabu",20,"bhaigade","B.sc IT");
+insert into women values(2,"aaksha",30,"patel","Engineering in IT");
+select * from women;
+
+create  database sportdb;
+use sportdb;
+create table athelete(id int, name varchar(20),sport varchar(30));
+insert into athelete values(1,"shrikant","cricket");
+insert into athelete values(2,"messi","football");
+insert into athelete values(3,"kamal","running");
+select*from athelete
+
+
+
