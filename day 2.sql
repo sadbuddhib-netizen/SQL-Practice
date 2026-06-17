0a9005547929c74c1a49be87162d473dd62840d5
sadbuddhib-netizen <sadbuddhib@gmail.com>
2026-06-17 20:51:59 +0530

    Add files via upload

---
+++ day 2.sql
@@ -0,0 +1,9 @@
+show databases;
+use om;
+select * from athelete
+select name from athelete;
+select sport from athelete;
+select name from athelete where name = "messi";
+select * from athelete where name = "messi";
+select * from athelete order by name  ASC;
+
